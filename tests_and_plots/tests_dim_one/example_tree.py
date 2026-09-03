33037278f0824aba37637a26a58584280fe7c3ab
thranduil1 <129960151+thranduil1@users.noreply.github.com>
2026-09-03 18:50:08 +0200

    plot example tree
    Added example_tree.py to plot a tree in 3D space using a family generated from make_family_one.

---
+++ tests_and_plots/tests_dim_one/example_tree.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dimension_one/making_tree import (
+    make_family_one,
+    tree_at_one,
+)
+
+from plot_dim_one_tree import plot_tree_in_cube
+
+
+family = make_family_one(
+    ambient_dimension=3,
+    junction_time=0.5,
+    angle_degrees=30.0,
+)
+
+x = np.array([0.5, 0.5])
+
+fig, ax, tree = plot_tree_in_cube(
+    family,
+    x,
+    elevation=20,
+    azimuth=-60,
+)
+
+fig.savefig(
+    "results/tree_plot_d1.png",
+    dpi=200,
+)
+
+plt.close(fig)
