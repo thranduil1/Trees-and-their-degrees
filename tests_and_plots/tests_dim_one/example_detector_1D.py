6ce19f1f9890fb65098f7490bd048def5e258233
thranduil1 <129960151+thranduil1@users.noreply.github.com>
2026-09-07 17:13:26 +0200

    Refactor example_detector_1D.py to use new heatmap function

--- tests_and_plots/tests_dim_one/example_detector_1D.py
+++ tests_and_plots/tests_dim_one/example_detector_1D.py
@@ -5,43 +5,33 @@ from dimension_one.making_tree import (
 from dimension_one.detector_one import make_slice_detector_map
 
 from detector_1D_plot import (
-    detector_heatmap,
+    detector_heatmap_N3,
     detector_vector_field,
 )
 
 import matplotlib.pyplot as plt
 
-
 family = make_family_one(
-    ambient_dimension=3,
-    junction_time=0.5,
+    N=3,
+    a=4,
     angle_degrees=30.0,
+    edge_length=1.0,
 )
 
-# Heat map
-fig1, ax1, grid, values = detector_heatmap(
+fig, ax, grid, values = detector_heatmap_N3(
     family,
+    fixed_coord="p3",
+    fixed_value=0.8,
     samples=101,
     cmap="viridis",
+    slice_time = 0.5,
 )
 
-fig1.savefig(
+plt.show()
+
+fig.savefig(
     "results/detector_heatmap.png",
     dpi=200,
 )
 
-plt.close(fig1)
-
-# Vector field
-fig2, ax2, X, Y, U, V = detector_vector_field(
-    family,
-    samples=21,
-    arrow_scale=0.15,
-)
-
-fig2.savefig(
-    "results/detector_vector_field.png",
-    dpi=200,
-)
-
-plt.close(fig2)
+plt.close(fig)
