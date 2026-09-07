11714590e4a587df3769318ec19918a0057ca534
thranduil1 <129960151+thranduil1@users.noreply.github.com>
2026-09-07 17:11:08 +0200

    tree example

--- tests_and_plots/tests_dim_one/example_tree.py
+++ tests_and_plots/tests_dim_one/example_tree.py
@@ -6,24 +6,25 @@ from dimension_one.making_tree import (
     tree_at_one,
 )
 
-from plot_dim_one_tree import plot_tree_in_cube
-
+from plot_dim_one_tree import plot_tree_3d
 
 family = make_family_one(
-    ambient_dimension=3,
-    junction_time=0.5,
+    N=3,
+    a=4,
     angle_degrees=30.0,
+    edge_length=1.0,
 )
 
-x = np.array([0.5, 0.5])
+p = np.array([0.2, 0.5, 0.6])  # some point in [0,1]^3
+tree = tree_at_one(family, p)
 
-fig, ax, tree = plot_tree_in_cube(
-    family,
-    x,
-    elevation=20,
-    azimuth=-60,
+fig, ax = plot_tree_3d(
+    tree,
+    show_vertices=True,
+    vertex_labels=True,
 )
 
+plt.show()
 fig.savefig(
     "results/tree_plot_d1.png",
     dpi=200,
