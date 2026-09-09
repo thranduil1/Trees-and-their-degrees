502c04573e399c75d5685c64a683c05a2a6ef91b
MCD <mariecamidelarue@gmail.com>
2026-09-09 17:58:35 +0200

    explanatory-doc

---
+++ examples/example_plot_some_trees.py
@@ -0,0 +1,59 @@
+## Some examples of trees plotted for different values. 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+from examples.examples_double.tree_plots import plot_tree_3d
+from double_junction.Tree_building import tree_at, make_tree_family
+
+family = make_tree_family(
+    a=3,
+    b=2,
+    N=3,
+)
+
+x = np.array([0.8, 0.8, 0.8])
+
+times = [0.0, 0.5, 1.0]
+
+titles = [
+    r"$t=0$: columns grouped first",
+    r"$t=1/2$: one ab-junction",
+    r"$t=1$: rows grouped first",
+]
+
+fig = plt.figure(figsize=(18, 6))
+
+for k, (t, title) in enumerate(
+    zip(times, titles),
+    start=1,
+):
+    ax = fig.add_subplot(
+        1,
+        3,
+        k,
+        projection="3d",
+    )
+
+    tree = tree_at(
+        family,
+        x=x,
+        t=t,
+    )
+
+    plot_tree_3d(
+        tree,
+        ax=ax,
+        title=title,
+        show_labels=True,
+    )
+
+plt.tight_layout()
+plt.show()
+
+plt.savefig(
+    "results/example_tree.png",
+    dpi=200,
+    bbox_inches="tight",
+)
+
+plt.close()
